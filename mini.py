3c63ac2088241b15518014a114c9a79bad96d586
Scott Sullivan <scott@sullysbrain.com>
2023-09-07 19:19:14 -0400

    Added required packages for LangChain to inspect PDFs

--- mini.py
+++ mini.py
@@ -1,28 +1,20 @@
-# Using Python, import all the json files in the _data folder
-# and merge them into a single file called data.json
+import mlflow
 
-import os, json
-import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
 
-#path_to_json = '_data/'
-#json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-#print(json_files)  # for me this prints ['foo.json']
+mlflow.autolog()
+
+db = load_diabetes()
+X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
+
+# Create and train models.
+rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
+rf.fit(X_train, y_train)
+
+# Use the model to make predictions on the test dataset.
+predictions = rf.predict(X_test)
 
 
-#with open('data.json', 'w') as f:
-#    json.dump(data, f)
 
-# Now, we can use the data.json file to create a Streamlit app
-# that will allow us to interact with the data.
-
-import streamlit as st
-import json
-
-with open('data.json', 'r') as f:
-    data = json.load(f)
-
-st.title('SullyGPT')
-input = st.text_input('Enter a prompt:')
-if input:
-    text = data[input]
-    st.write(text)
