41fba6f280975601ddc19cb0da850474019d8d61
Scott Sullivan <scott@sullysbrain.com>
2023-09-11 17:36:11 -0400

    First AWS python code run on EC2. Pretty cool.

---
+++ aws_test.py
@@ -0,0 +1,7 @@
+# in prep for running llama2 on AWS, this is a test for uploading and running on an EC2 instance
+
+import numpy as np
+
+random_number = np.random.randint(100, size=10)
+
+print(f'Random number: {random_number}')
